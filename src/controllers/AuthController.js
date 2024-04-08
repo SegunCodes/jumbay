29e9ba3ff43a75f5e0238319b55dd9ae9c4997ed
SegunCodes <shegstix64@gmail.com>
2024-04-08 21:30:42 +0100

    feat: commence register user method

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,1 +1,24 @@
-// all authentication processes happen here
+const bcryptjs = require("bcryptjs")
+//method for creating a user
+exports.registerUser = async (req, res) => {
+    try {
+        const { email, usertype, password } = req.body
+        // check if user exists in our database
+        // if user exists, return an error
+        return res.status(400).json({
+            status : false,
+            data : [],
+            message : "User not found"
+        })
+
+        //if user does not exist, we hash the password
+        const hashed_password = await bcryptjs.hash(password, 10);
+        //store user details to database.
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// exports.loginUser = async (req, res) => {
+    
+// }
