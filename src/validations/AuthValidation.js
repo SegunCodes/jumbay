b92406e979afa52a300f27da0b1a88e055a280e0
Churchill <churchillrimamskep@gmail.com>
2024-04-13 21:46:53 +0100

    made the functionality for the loginUser using get request i made some validation to check against the task and give appropriate message

--- src/validations/AuthValidation.js
+++ src/validations/AuthValidation.js
@@ -1,7 +1,12 @@
-const joi = require("joi")
+const joi = require("joi");
 
 exports.RegisterValidation = joi.object({
-    email: joi.string().email().required(),
-    usertype: joi.string().valid('buyer', 'seller').required(),
-    password: joi.string().required()
-})
+  email: joi.string().email().required(),
+  usertype: joi.string().valid("buyer", "seller").required(),
+  password: joi.string().required(),
+});
+
+exports.loginValidation = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
