34ccab07101447d96155da5ed5470eb4b78032b1
Churchill <churchillrimamskep@gmail.com>
2024-04-14 17:44:58 +0100

    made the functionality for the loginUser using get request i made some validation to check against the task and give appropriate message

--- src/middlewares/ValidationMiddleware.js
+++ src/middlewares/ValidationMiddleware.js
@@ -1,11 +1,25 @@
 exports.validateSchema = (schema) => (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if(error){
-        return res.status(422).json({
-            status : false,
-            data : [],
-            message : error.details[0].message
-        }) 
-    }
-    next();
-}
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(422).json({
+      status: false,
+      data: [],
+      message: error.details[0].message,
+    });
+  }
+
+  next();
+};
+
+// exports.validateLoginSchema = (schema) => (req, res, next) => {
+//   const { error } = schema.validate(req.body);
+//   if (error) {
+//     return res.status(422).json({
+//       status: false,
+//       data: [],
+//       message: error.details[0].message,
+//     });
+//   }
+//   // If validation passes, continue with the next middleware or route handler
+//   next();
+// };
--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,10 +1,23 @@
 // all authentication routes come here
-const express = require("express")
-const AuthController = require("../controllers/AuthController")
-const validator = require("../middlewares/ValidationMiddleware")
-const { RegisterValidation } = require("../validations/AuthValidation")
-const authRoutes = express.Router()
+const express = require("express");
+const AuthController = require("../controllers/AuthController");
+const validator = require("../middlewares/ValidationMiddleware");
+const {
+  RegisterValidation,
+  loginValidation,
+} = require("../validations/AuthValidation");
+const authRoutes = express.Router();
 
-authRoutes.post("/registerUser", validator.validateSchema(RegisterValidation), AuthController.registerUser)
+authRoutes.post(
+  "/registerUser",
+  validator.validateSchema(RegisterValidation),
+  AuthController.registerUser
+);
+
+authRoutes.get(
+  "/loginUser",
+  validator.validateSchema(loginValidation),
+  AuthController.loginUser
+);
 
 module.exports = authRoutes;
