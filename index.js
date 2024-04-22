6f492974d841ce3c3e009ac75d3a7b9e9ecafd53
SegunCodes <shegstix64@gmail.com>
2024-04-22 22:12:57 +0100

    usr product flow and auth middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config()
 const cors = require("cors");
 const authRoutes = require("./src/routes/AuthRoutes");
+const userRoutes = require("./src/routes/UserRoutes");
 const port = process.env.PORT || 4006
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.json({status:"success"})
 })
+app.use('/user', userRoutes)
 app.use('/auth', authRoutes)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
